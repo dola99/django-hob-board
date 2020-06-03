ee13346e3c2d711d5f4a65476dd44878d1d95e23
Mohamed Adel <Haaam25@gmail.com>
2020-06-03 22:31:00 +0200

    add views jobs views &url

--- job/admin.py
+++ job/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Job
+from .models import Job  , Category
 
 admin.site.register(Job)
+admin.site.register(Category)
+ 
--- job/models.py
+++ job/models.py
@@ -16,6 +16,14 @@ class Job(models.Model):
     vacany = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     exprience = models.IntegerField(default=1)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+class Category (models.Model):
+
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
